3df83fab905d6b72649ee90dcd14c03fe3fc99a1
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-16 10:27:06 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#Include <stdio.h>
+/**
+ * *main entry point
+ * *
+ * *Description "program that prints with puts function 
+ * *
+ * *return always (0);
+ * */
+Int main (void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
+}
