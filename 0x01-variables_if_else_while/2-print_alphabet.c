73df20199b85ea1eca1df518a96a4dd9541a995c
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-17 16:18:27 -0700

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main-print lower case aphabets.
  *
@@ -6,6 +7,7 @@
  *
  * Return:Always 0(Success)
 */
+
 int main(void)
 {
 char alp[26] = "abcdefghijklmnopqrstuvwxyz";
