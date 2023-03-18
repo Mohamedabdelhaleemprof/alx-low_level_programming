7d93b9aa1e0bf6e285f402f4a9471492d5a18df1
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-17 18:10:17 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#includee <stdio.h>
+/**
+ *main-print lower case aphabets.
+ *
+ *Description : return value of 'main' is Return.
+ *
+ *Return:Always 0(Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar([i]);
+}
+putchar('\n');
+return (0);
+}
