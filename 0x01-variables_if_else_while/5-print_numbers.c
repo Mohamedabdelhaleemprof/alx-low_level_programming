d34f60e4b79a6d66b2a8d6f1b68ed1d64352f4d1
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-17 17:53:39 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main-print single decimal numbers.
+ *
+ *Description : return value of 'main' is Return.
+ *
+ * Return:Always 0(Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 9; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
