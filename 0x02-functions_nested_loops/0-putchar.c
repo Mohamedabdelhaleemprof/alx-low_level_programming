e04280d9a21b60b7dadbee1ea365c61c0364f9bc
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-21 16:11:24 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,22 @@
 #include "main.h"
 
+/**
+ *main - print _putchar
+ *
+ * is written by mbah
+ *
+ * Return: 0
+*/
+
 int main(void)
 {
-char a [7] = {_putchar};
-putchar ("%c\n", a);
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
 return (0);
 }
