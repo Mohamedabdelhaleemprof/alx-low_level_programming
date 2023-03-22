5d176d03473e6a60edd58f88ac79e0b2291e62e1
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-22 13:37:56 -0700

    a

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,33 @@
-iuu
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print to 98
+ *
+ * Return: Always n
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n < 99; n++)
+	{
+		_putchchar('n');
+		_putchchar(',');
+		_putchchar(' ');
+		_putchchar('\n');
+	}
+
+	for (n < 112; x = 98; x--)
+	{
+		_putchchar('n');
+		_putchchar(',');
+		_putchchar(' ');
+		_putchchar('\n');
+	}
+	for (n = -10; x < 99; x++)
+	{
+		_putchchar('n');
+		_putchchar(',');
+		_putchchar(' ');
+		_putchchar('\n');
+	}
+}
