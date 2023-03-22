aa34e42596c2debba50cf54946a82db28babbfa0
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-22 15:01:22 -0700

    a

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,8 @@
 
 /**
  * print_to_98 - print to 98
- *
+ *followed by new line
+ *@n: print from this number
  */
 
 void print_to_98(int n)
@@ -14,9 +15,9 @@ if (n <= 98)
 for (i = n; i < 99; i++)
 {
 if (i != 98)
-printf("%d, \n", i);
+printf("%d, ", i);
 else if (i == 98)
-printf("%d, \n", i);
+printf("%d\n", i);
 }
 }
 
@@ -25,9 +26,9 @@ else if (n > 98)
 for (j = n; j <112; j++)
 {
 if (j != 98)
-printf("%d, \n", j);
+printf("%d, ", j);
 else if (j ==98)
-printf("%d, \n", j);
+printf("%d\n", j);
 }
 }
 }
