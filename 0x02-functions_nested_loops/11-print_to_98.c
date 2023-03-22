2836b71238a7a9fa96ba7c8fa6f9881302caa511
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-22 14:00:52 -0700

    a

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,15 +10,15 @@ void print_to_98(int n)
 {
 for (n = 0; n < 99; n++)
 {
-printf("%d, \n", n)
+printf("%d, \n", n);
 }
 
 for (n < 112; n = 98; x--)
 {
-printf("%d, \n", n)
+printf("%d, \n", n);
 }
 for (n = -10; n < 99; x++)
 {
-printf("%d, \n", n)
+printf("%d, \n", n);
 }
 }
