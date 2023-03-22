ac358d7f7954bf99397e52cdf98c1eb70ac312da
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-22 11:56:06 -0700

    a

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,15 @@
-iiuufd:t
+
+#include "main.h"
+/**
+ * add - add two integers
+ * @a : number to be added to b
+ * @b : number to be added to b
+ *
+ * Return: Always n
+ */
+int add(int a, int b)
+{
+int n;
+n = a + b;
+return (n)
+}
