c4c7783e47ca2a97e794846e141421db4bbe7241
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-22 09:31:08 -0700

    a

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,14 @@
- gg
+#include "main.h"
+/**
+ * jack_bauer - print every minute
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int a;
+	for (a >= 00:00; a < 24:00; a++)
+	{
+		_putchar('%d\n', a);
+	}
+}
