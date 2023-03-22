af41954a7e75b061f7e9b1d2d26bcc01cdc42e99
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-22 07:55:43 -0700

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,25 @@
 #include "main.h"
+
 /**
- * print_last_digit - print last digit of the number 
+ * print_last_digit - print last digit of the number
+ *
+ *@n: is the last digit number
  *
  * Return: n if it is equal or more than 0,otherwise it will be -n.
 */
+
 int print_last_digit(int n)
 {
 	int a;
+
 	int x;
-	for (x <= 0;x > 0; x++)
+
 	a = n + x;
+
 	x = a - n;
+x <= 0;
+x > 0;
 	if (n > 0)
-
-
 	{
 		return (n);
 	}
