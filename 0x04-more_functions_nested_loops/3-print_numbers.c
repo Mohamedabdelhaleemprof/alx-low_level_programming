530d7fd5f5945acd62d1b3a6c199df1d596f02d1
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-23 14:38:57 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0-9
+ *
+ * @i: numbers from 0 - 9
+ */
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+_putchar('i');
+}
+putchar('\n');
+}
