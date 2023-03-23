078c47db92ee4cefce7568527f6b1951132f49d8
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-23 12:55:49 -0700

    a

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -3,11 +3,12 @@
 
 /**
  * _putchar - write the character c to stdout
- * @c : the character to be printed
- * Return : on success 1 
- * on error, -1 is returned and errno is set appropriately
+ * @c : the character to print
+ * Return : On success 1 
+ * On error, -1 is returned, and errno is set appropriately,
  */
-int_putchar(char c)
+
+int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
