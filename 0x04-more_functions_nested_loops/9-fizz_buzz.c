5d0fde47a9d975bf6c2fc7eb4e88a5a4f0cc578a
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-24 05:18:20 -0700

    a

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print fizz
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else
+		{
+			    printf("%d ", i);
+		}
+	}
+	return (0);
+}
