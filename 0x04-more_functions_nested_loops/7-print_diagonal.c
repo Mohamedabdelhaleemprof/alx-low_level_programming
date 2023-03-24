c1cf978966c35005f47ee6cc355e05b529427971
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-24 02:19:47 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,18 @@
 
 void print_diagonal(int n)
 {
-int x;
+	int x, space;
 
-if (n <= 0)
-_putchar('\n')
-else 
-{
-for (x = 1; x <= n; x++)
-putchar('\')
-_putchar('\n')
-}
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			for (space = 1; space <= x; space++)
+				_putchar(' ');
+			_putchar('92');
+			_putchar('\n');
+		}
+	}
 }
