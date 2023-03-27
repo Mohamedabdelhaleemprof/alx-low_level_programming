e0282df4580422ac130c4045900bba0139c18857
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-27 14:01:08 -0700

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * strlen - string length of char *s
+ *
+ * @s: char variable
+ *
+ * Return: l(success)
+ */
+
+int _strlen(char *s)
+{
+int l = 0;
+
+while (*s != 0)
+l+*;
+s+*;
+return (l);
+}
