bd852d24f2c8be11518506306b55d3273a14ae45
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-30 16:02:39 -0700

    a

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ * @dest: destinatio wordn.
+ * @src: source word.
+ * @n: size of bytes.
+ * Return: (dest)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i])
+		i++;
+	for (j = 0;j < n && src[j] != '\0'; j++)
+	dest[i+j] = src[j];
+	dest[i+j] = '\0';
+	return (dest);
+}
