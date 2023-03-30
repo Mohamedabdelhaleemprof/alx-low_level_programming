e42735a04e48a03732604b9ca730bb6dc469c936
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-03-30 12:36:59 -0700

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings.
+ *
+ * *dest: destination
+ * *src: source
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	i = 0;
+
+	while (dest[i];)
+		i++;
+	for (j = 0; src[j]; j++)
+		dest[i++] = src[j];
+	return (dest);
+}
