dc168c456c5ed68cb820732dfa5b7233a5681228
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-04 07:17:56 -0700

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,11 +11,11 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-int x, j;
+unsigned int x, j;
 
 for (x = 0; s[x] != '\0'; x++)
 {
-for (j = 0; accept[j] = s[x]; j++)
+for (j = 0; accept[j] != s[x]; j++)
 {
 if (accept[j] == '\0')
 return (x);
