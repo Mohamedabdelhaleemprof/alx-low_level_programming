7efa58fba9195402e5452d91126498041df89dfe
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-04 13:19:02 -0700

    a

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,24 +13,24 @@ char *_strpbrk(char *s, char *accept)
 {
 int x, j, i, f = 0;
 
-for (x = 0; s[x] != '\0'; x++)
-{
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (accept[j] == s[x])
-{
-f = 1;
-break;
-}
-}
-if (f == 1)
-{
-break;
-}
-}
-for (i = 0; s[x] != '\0'; i++)
-{
-result[i] = s[x++];
-}
-return (result);
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+		if (accept[j] == s[x])
+		{
+		f = 1;
+			break;
+		}
+		}
+		if (f == 1)
+		{
+			break;
+		}
+	}
+	for (i = 0; s[x] != '\0'; i++)
+	{
+		result[i] = s[x++];
+	}
+	return (result);
 }
