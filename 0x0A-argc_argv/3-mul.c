cf6988018f1a2cfd3264699787cd453f27b01429
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-09 08:46:14 -0700

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,12 @@ int x;
 
 if (argc == 3)
 {
-x = atoi(argv[1]) * atoi(argv[2];
+x = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", x);
 }
 else
 {
-printf("error\n");
+printf("Error\n");
 return 1;
 }
 return (0);
