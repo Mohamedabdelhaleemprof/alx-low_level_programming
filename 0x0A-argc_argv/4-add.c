9567ce7d31ad811d5834cd86a1f57c33b2ffd185
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-09 16:53:00 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,15 +12,16 @@ int main(int argc, char *argv[])
 {
 intt x = 0;
 char *c;
+
 while (--argc)
 {
-for (c = argv[argc]; *c; c++)
-{
-if (*c < '0' || *c > '9')
-{
-return (printf("Error\n"), 1);
-}
-}
+	for (c = argv[argc]; *c; c++)
+	{
+		if (*c < '0' || *c > '9')
+		{		
+			return (printf("Error\n"), 1);
+		}
+	}
 x += atoi(argv[argc]);
 }
 printf("%d\n", x);
