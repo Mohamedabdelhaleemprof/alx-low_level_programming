3ad14da1c446db872eec73d4df9924ee75fe88db
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-09 08:32:01 -0700

    a

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - locates a substring
+ *
+ *@argc:count of strings
+ *@argv: the string name
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int x;
+if (argc < 3)
+{
+printf("error\n");
+return 1;
+}
+else
+{
+x = atoi(argv[1]) * atoi(argv[2];
+printf("%d\n", x); 
+}
+return (0);
+}
