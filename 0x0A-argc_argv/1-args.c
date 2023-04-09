2adabf29350c2af0593030c7db11cbb2aa418e1e
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-09 07:05:36 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - locates a substring
+ *
+ *@argc:count of strings
+ *@argv: the string name
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
