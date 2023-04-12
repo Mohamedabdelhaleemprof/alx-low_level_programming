3c9a576fb9fc3212b84c41bd28ea46f9f01ab3c5
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-11 17:38:13 -0700

    a

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+nclude <stdio.h>
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	    char *x;
+	        int i = 0, f =0, j=0, n, d;
+		    
+		    if (s1 == NULL || s2 == NULL )
+			            return NULL;
+		        
+		        for(i = 0; s1[i] != '\0'; i++);
+			    for(f = 0; s2[f] != '\0'; f++);
+			        
+			        x = (char *)malloc(((i + f + 1) * sizeof (char)));
+				    
+				    if (x != NULL){
+					            n = i+f;
+						            d = f-i;
+							            for(j = 0; j < n; j++)
+									            {
+											                if(j < i)
+														            {
+																                    x[j] = s1[j];
+																		                }
+													            else if (j < n)
+															                {
+																		                x[j] = s2[j-f+d];
+																				            }
+														            }
+								        }
+				        x[n] = '\0';
+					    return x;
+}
+
+
