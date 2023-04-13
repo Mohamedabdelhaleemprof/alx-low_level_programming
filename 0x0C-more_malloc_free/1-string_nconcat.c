1bf62e6a771fabc487ba820df5d1b48a52cc3e8e
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-13 15:11:26 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,31 +16,31 @@ char *a;
 
 if (s1 == NULL)
 {
-x = 0;
+	x = 0;
 }
 else
 {
-for (x = 0; s1[x]; ++x)
+	for (x = 0; s1[x]; ++x)
 	;
 }
 if (s2 == NULL)
 {
-y = 0;
+	y = 0;
 }
 else
 {
-for (y = 0; s2[y]; ++y)
+	for (y = 0; s2[y]; ++y)
 	;
 }
 if (y > n)
-y = n;
+	y = n;
 a = malloc(sizeof(char) * (x + y + 1));
 if (a == NULL)
-return (NULL);
+	return (NULL);
 for (z = 0; z < x; z++)
 	a[z] = s1[z];
 for (z = 0; z < y; z++)
 	a[z + x] = s2[z];
-	a[x + y] = '\0';
+a[x + y] = '\0';
 return (a);
 }
