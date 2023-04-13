6ae506fb5017643e1da8c4aee162980ee512f878
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-13 16:03:14 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,39 @@
-nclude "main.h"
+#include "main.h"
 
-/**
- *  * string_nconcat - a function that concatenates two strings.
- *   *
- *    * @s1: first char
- *     * @s2: secound char
- *      * @n: unsigned int
- *       *
- *        * Return: If the function fails, it should return NULL
- *         */
+ /**
+  * string_nconcat - a function that concatenates two strings.
+  *
+  * @s1: first char
+  * @s2: secound char
+  * @n: unsigned int
+  *
+  * Return: If the function fails, it should return NULL
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-		unsigned int x, y, z;
-			char *s;
+unsigned int x, y, z, e;
+char *a;
 
-				if (s1 == NULL)
-						{
-									x = 0;
-										}
-					else
-							{
-										for (x = 0; s1[x]; ++x)
-													;
-											}
-						if (s2 == NULL)
-								{
-											y = 0;
-												}
-							else
-									{
-												for (y = 0; s2[y]; ++y)
-															;
-													}
-								if (y > n)
-											y = n;
-									s = malloc(sizeof(char) * (x + y + 1));
-										if (s == NULL)
-													return (NULL);
-											for (z = 0; z < x; z++)
-														s[z] = s1[z];
-												for (z = 0; z < y; z++)
-															s[z + x] = s2[z];
-													s[x + y] = '\0';
-														return (s);
+if (s1 == NULL)
+{
+	s1 = "";
+}
+if (s2 == NULL)
+{
+	s2 = "";
+}
+for (x = 0; s1[x] != '\0'; x++)
+	;
+for (y = 0; s2[y] != '\0'; ++y)
+	;
+a = malloc(sizeof(char) * (x + n + 1));
+if (a == NULL)
+	return (NULL);
+for (z = 0; s1[z] != '\0'; z++)
+	a[z] = s1[z];
+for (e = 0; e < n; e++)
+	a[z] = s2[e];
+	z++;
+a[z] = '\0';
+return (a);
 }
