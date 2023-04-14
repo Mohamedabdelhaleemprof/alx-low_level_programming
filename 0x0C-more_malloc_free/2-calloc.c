804daaf9c5fde35ba7180d9a73234be561b76d52
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-14 10:43:37 -0700

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,7 +7,7 @@
  * @nmemb: allocate memory for array
  * @size: allocate element of size bytes
  *
- * Return: pointer to the allocated memory.
+ * Return: pointer 
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
