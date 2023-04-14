82b0f6fe2646d921c8520913e9617380c4c6ad40
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-14 12:22:26 -0700

    a

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int *array_range(int min, int max)
+{
+int *a;
+int d = (max - min) + 1
+a = malloc(d * sizeof(int));
+if (a == null)
+	return (null);
+if (min >= max)
+	return (null);
+else if (min < max)
+int x;
+for (x = min; x > max; x++)
+	a[x] = 0;
+return (a);
