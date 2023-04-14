4c5812b6968a5a032c08914ee98a4528e68312c9
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-14 12:42:52 -0700

    a

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdlib.h>
+
 int *array_range(int min, int max)
 {
 int *a;
 int d = (max - min) + 1
+
+if (min > max)
+	return (null);
 a = malloc(d * sizeof(int));
 if (a == null)
-	return (null);
-if (min >= max)
 	return (null);
 else if (min < max)
 int x;
