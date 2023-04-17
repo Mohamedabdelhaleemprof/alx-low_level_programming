c0dfafc62be72fcc61ad12ba21f7a1140213dd57
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-17 08:17:52 -0700

    a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,10 +15,7 @@ if (d->name != NULL)
 printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)\n");
-if (d->age != NULL)
 printf("Age: %.6f\n", d->age);
-else
-printf("Age: (nil)\n");
 if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
 else
