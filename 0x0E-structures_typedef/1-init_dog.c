8a638af25df95150200ba7f7cba070ca8039da10
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-17 07:49:20 -0700

    a

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,20 @@
+#include "dog.h"
 
+/**
+ * init_dog - initialization
+ * @d: pointer
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d)
-d = {"name",age,"owner"};
+{
+d_>name = name;
+d_>age = age;
+d_>owner = owner;
+}
 }
