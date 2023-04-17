03d394c49c47ac2b93337345058dd6a434f98f1e
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-17 09:06:23 -0700

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,33 +15,33 @@ unsigned int x, y, z;
 dog_t *k;
 
 if (name == NULL || owner == NULL)
-return (NULL);
+	return (NULL);
 k = malloc(sizeof(dog_t));
 if (k == NULL)
-return (NULL);
+	return (NULL);
 for (x = 0; name[x]; x++)
-		;
+	;
 x++;
 k->name = malloc(x * sizeof(char));
 if (k->name == NULL)
 {
-free(k);
-return (NULL);
+	free(k);
+	return (NULL);
 }
 for (z = 0; z < x; z++)
-k->name[z] = name[z];
+	k->name[z] = name[z];
 k->age = age;
 for (y = 0; owner[y]; y++)
 	;
 y++;
-k->owner = malloc(ol * sizeof(char));
+k->owner = malloc(y * sizeof(char));
 if (k->owner == NULL)
 {
-free(k->name);
-free(k);
-return (NULL);
+	free(k->name);
+	free(k);
+	return (NULL);
 }
 for (z = 0; z < y; z++)
-dog->owner[i] = owner[i];
+	dog->owner[z] = owner[z];
 return (k);
 }
