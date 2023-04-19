b5368374ab4a45fd1ff558e85da6087a0d1d7e01
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-04-19 14:46:34 -0700

    a

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ *int_index - index
+ *@array: array
+ *@size:size
+ *@cmp:function
+ *Return: 0 or -1
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int x = 0;
+
+if (size > 0)
+{
+if (array != NULL && cmp != NULL)
+{
+	while (i < size)
+	{
+		if (cmp(array[x]))
+			return (x);
+		x++;
+	}
+}
+}
+return (-1);
+}
