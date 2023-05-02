712af1ff254582a9ff2dc2823817c737ba267eb7
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-05-02 06:41:44 -0700

    a

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+nclude "lists.h"
+
+/**
+ * reverse_listint - reverse
+ * @head: double pointer
+ * Return: head
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *m, *t;
+if (head == NULL || *head == NULL)
+return (NULL);
+if ((*head)->next == NULL)
+return (*head);
+m = NULL;
+while (*head != NULL)
+{
+t = (*head)->next;
+(*head)->next = m;
+m = *head;
+*head = t;
+}
+*head = m;
+return (*head);
+}
