94b515c539b6bdf3a77d3ac1d3ff0828429466be
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-05-03 07:44:47 -0700

    a

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+nclude <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *  list_len - list
+ *   @h: head
+ *    Return: t
+ */
+size_t list_len(const list_t *h)
+{
+int t = 0;
+while (h)
+{
+t++;
+h = h->next;
+}
+return (t);
+}
