5f4c3661cefc319424f7ffe6c7e5e9d6d5e18cda
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-05-03 09:26:46 -0700

    a

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *  _strlen - length
+ *   @str: str
+ *    Return:
+ */
+unsigned int _strlen(char *str)
+{
+unsigned int x;
+for (x = 0; str[x]; x++)
+;
+return (x);
+}
+
+/**
+ *  add_node_end - add end node
+ *   @head: head
+ *    @str: str
+ *     Return: f
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *f, *s;
+if (str == NULL)
+return (NULL);
+f = malloc(sizeof(list_t));
+if (f == NULL)
+return (NULL);
+f->str = strdup(str);
+if (f->str == NULL)
+{
+free(f);
+return (NULL);
+}
+f->len = _strlen(f->str);
+f->next = NULL;
+if (*head == NULL)
+{
+*head = f;
+return (f);
+}
+s = *head;
+while (s->next)
+s = s->next;
+s->next = f;
+return (f);
+}
