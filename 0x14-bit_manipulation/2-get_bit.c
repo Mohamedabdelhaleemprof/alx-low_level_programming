ed5296f2bec03e39bcef3eb2f079b5a3160e974b
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-05-04 11:41:38 -0700

    a

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,9 +6,10 @@
  *@index: index
  * Return: result
  */
-int get_bit(unsigned long int n, unsigned int index) {
-	    if (index >= sizeof(unsigned long int) * 8) {
-		            return -1;
-			        }
-	        return (n >> index) & 1;
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8) {
+return -1;
+}
+return (n >> index) & 1;
 }
