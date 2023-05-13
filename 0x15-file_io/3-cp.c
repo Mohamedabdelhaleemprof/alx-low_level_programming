2d566305ee7b86326227af2c00ed3a2134ec602a
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-05-13 07:54:17 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,21 +21,24 @@ if (argc != 3)
 {
 print_error_and_exit(97, "Usage: cp file_from file_to\n", "", -1);
 }
-int file_from_fd = open(argv[1], O_RDONLY);
+int file_from_fd;
+file_from_fd = open(argv[1], O_RDONLY);
 if (file_from_fd == -1) {
 print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1], -1);
 }
-int file_to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+int file_to_fd;
+file_to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (file_to_fd == -1)
 {
 print_error_and_exit(99, "Error: Can't write to %s\n", argv[2], file_from_fd);
 }
-char buffer[BUFFER_SIZE];
+char buffer[1024];
 ssize_t read_count, write_count;
 while ((read_count = read(file_from_fd, buffer, BUFFER_SIZE)) > 0)
 {
 write_count = write(file_to_fd, buffer, read_count);
-if (write_count != read_count) {
+if (write_count != read_count)
+{
 print_error_and_exit(99, "Error: Can't write to %s\n", argv[2], file_from_fd);
 }
 }
@@ -51,5 +54,5 @@ if (close(file_to_fd) == -1)
 {
 print_error_and_exit(100, "Error: Can't close fd %d\n", "", file_from_fd);
 }
-return 0;
+return (0);
 }
