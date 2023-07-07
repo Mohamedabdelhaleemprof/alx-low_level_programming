16ee2196716c1ccde443c1c66b76dc3ec4bebfe5
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-07-07 09:12:42 -0700

    a

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - the bit
+ * @index: index
+ * @n: pointer
+ * Return: 1 , or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int a;
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	a = 1 << index;
+	*n = *n | a;
+
+	return (1);
+}
