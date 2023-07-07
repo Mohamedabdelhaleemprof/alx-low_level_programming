22d288f4eccb77b37fd9c43f209f5219cc69714c
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-07-07 08:56:46 -0700

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int j;
-j = 0;
+int c;
+unsigned int d;
+d = 0;
 if (!b)
 return (0);
-for (i = 0; b[i] != '\0'; i++)
+for (c = 0; b[c] != '\0'; c++)
 {
-if (b[i] != '0' && b[i] != '1')
+if (b[c] != '0' && b[c] != '1')
 return (0);
 }
-for (i = 0; b[i] != '\0'; i++)
+for (c = 0; b[c] != '\0'; c++)
 {
-j <<= 1;
-if (b[i] == '1')
-j += 1;
+d <<= 1;
+if (b[c] == '1')
+d += 1;
 }
-return (j);
+return (d);
 }
