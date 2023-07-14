2278aff6f299396a120d35008fbf6ac69afd8db6
Mohamedabdelhaleemprof <mohamed.abdelhaleem.prof@gmail.com>
2023-07-14 14:17:03 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
-  * ad - ad
+ * ad - ad
  * @a: pointer
  *
  * Return: b
  */
 size_t add(char *a)
 {
-	size_t b;
-
-	for (b = 0; a[b]; b++)
-		;
-	return (b);
+size_t b;
+for (b = 0; a[b]; b++)
+;
+return (b);
 }
 
 /**
@@ -24,18 +23,17 @@ size_t add(char *a)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int c;
-	ssize_t d;
-
-	if (filename == NULL)
-		return (-1);
-	c = open(filename, O_WRONLY | O_APPEND);
-	if (c == -1)
-		return (-1);
-	if (text_content != NULL)
-		d = write(c, text_content, add(text_content));
-	close(c);
-	if (d == -1)
-		return (-1);
-	return (1);
+int c;
+ssize_t d;
+if (filename == NULL)
+return (-1);
+c = open(filename, O_WRONLY | O_APPEND);
+if (c == -1)
+return (-1);
+if (text_content != NULL)
+d = write(c, text_content, add(text_content));
+close(c);
+if (d == -1)
+return (-1);
+return (1);
 }
